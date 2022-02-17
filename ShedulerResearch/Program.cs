aa21f7e0ff3f29f21510d7e3a8cf26d1130610fe
Kibnet Philosoff <kibnet@hotmail.com>
2022-02-17 16:14:43 +0300

    some changes

--- ShedulerResearch/Program.cs
+++ ShedulerResearch/Program.cs
@@ -1,11 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using CronExpressionDescriptor;
+using Cronos;
 
 void GetCronDescription(string description, string pattern)
 {
+    //Перевод в человекочитаемый вид
     var output = CronExpressionDescriptor.ExpressionDescriptor.GetDescription(pattern, new Options { Locale = "ru" });
     Console.WriteLine($"{description} -> {pattern} -> {output}");
+    CronExpression expression = CronExpression.Parse(pattern);
+
+    //Получение следующей даты срабатывания
+    DateTime? nextUtc = expression.GetNextOccurrence(DateTime.UtcNow);
+    Console.WriteLine($"Next occurrence - {nextUtc}");
+
+    //Получение дат срабатывания в период времени
+    var list = expression.GetOccurrences(DateTime.Today, DateTime.Today.AddMonths(1));
+    Console.WriteLine($"Period occurrences - {string.Join(",", list)}"); 
+
+    //Шаблон расписания, требующий уточнения
 }
 
 
